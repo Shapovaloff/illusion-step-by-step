b441a61e3be84e471f44858c9a635559f97be830
Yury <mr.Shapovaloff3101@gmail.com>
2022-05-23 20:24:19 +0300

    4.3 Добавляем вспомогательные функции

--- source/js/helpers/animate.js
+++ source/js/helpers/animate.js
@@ -1,3 +1,19 @@
+// animation using raf (render function parameter is value from easing function)
+const animateEasing = (render, duration, easing) => new Promise((resolve) => {
+  const start = Date.now();
+  (function loop() {
+    const p = (Date.now() - start) / duration;
+    if (p > 1) {
+      render(1);
+      // set that animation end
+      resolve();
+    } else {
+      requestAnimationFrame(loop);
+      render(easing(p));
+    }
+  }());
+});
+
 // animation using raf (render function parameter is progress from 0 to 1)
 const animateProgress = (render, duration) => new Promise((resolve) => {
   const start = Date.now();
@@ -14,4 +30,20 @@ const animateProgress = (render, duration) => new Promise((resolve) => {
   }());
 });
 
-export {animateProgress};
+// animation using raf (render function parameter is progress from 0 to 1)
+const animateDuration = (render, duration) => new Promise((resolve) => {
+  const start = Date.now();
+  (function loop() {
+    const p = Date.now() - start;
+    if (p > duration) {
+      render(duration);
+      // set that animation end
+      resolve();
+    } else {
+      requestAnimationFrame(loop);
+      render(p);
+    }
+  }());
+});
+
+export {animateEasing, animateProgress, animateDuration};
